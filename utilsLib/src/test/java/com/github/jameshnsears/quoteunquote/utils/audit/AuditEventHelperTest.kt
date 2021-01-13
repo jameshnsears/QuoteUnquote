b71a02ed8497e54ebb016f4f778223dd4652ce63
James Sears <james.hn.sears@gmail.com>
2021-01-13 15:06:47 +0200

    ci appcenter.ms

--- utilsLib/src/test/java/com/github/jameshnsears/quoteunquote/utils/audit/AuditEventHelperTest.kt
+++ utilsLib/src/test/java/com/github/jameshnsears/quoteunquote/utils/audit/AuditEventHelperTest.kt
@@ -3,6 +3,8 @@ package com.github.jameshnsears.quoteunquote.utils.audit
 import android.os.Build
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.github.jameshnsears.quoteunquote.utils.BuildConfig
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
@@ -16,5 +18,13 @@ class AuditEventHelperTest {
         AuditEventHelper.createInstance(getApplicationContext())
         val properties: ConcurrentHashMap<String, String> = ConcurrentHashMap()
         AuditEventHelper.auditEvent("", properties)
+
+        assertTrue(BuildConfig.APPCENTER_KEY.length == 36)
+
+        if (BuildConfig.DEBUG) {
+            assertTrue(BuildConfig.APPCENTER_KEY.startsWith("5f602004"))
+        } else {
+            assertTrue(BuildConfig.APPCENTER_KEY.startsWith("32ce9b2c"))
+        }
     }
 }
