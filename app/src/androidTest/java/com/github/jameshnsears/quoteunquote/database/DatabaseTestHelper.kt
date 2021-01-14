37b578bb507cb9409f39769c5360b44d144c199f
James Sears <james.hn.sears@gmail.com>
2021-01-14 17:57:34 +0200

    fix ci issues

--- app/src/androidTest/java/com/github/jameshnsears/quoteunquote/database/DatabaseTestHelper.kt
+++ app/src/androidTest/java/com/github/jameshnsears/quoteunquote/database/DatabaseTestHelper.kt
@@ -1,9 +1,6 @@
 package com.github.jameshnsears.quoteunquote.database
 
-import android.content.Context
-import androidx.test.core.app.ApplicationProvider
 import com.github.jameshnsears.quoteunquote.QuoteUnquoteModelDouble
-import com.github.jameshnsears.quoteunquote.database.history.AbstractDatabaseHistory
 import com.github.jameshnsears.quoteunquote.database.quotation.QuotationEntity
 import com.github.jameshnsears.quoteunquote.utils.ContentSelection
 import com.github.jameshnsears.quoteunquote.utils.widget.WidgetIdHelper
@@ -29,43 +26,26 @@ open class DatabaseTestHelper {
     fun insertTestDataSet01() {
         val quotationEntityList: MutableList<QuotationEntity> = ArrayList()
         quotationEntityList.add(
-                QuotationEntity(
-                        DatabaseRepository.DEFAULT_QUOTATION_DIGEST, "a0", "q0"
-                ))
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d1", "a1", "q1"
-                ))
+            QuotationEntity(
+                DatabaseRepository.DEFAULT_QUOTATION_DIGEST, "a0", "q0"
+            )
+        )
+        quotationEntityList.add(QuotationEntity("d1", "a1", "q1"))
         databaseRepositoryDouble.insertQuotations(quotationEntityList)
     }
 
     fun insertTestDataSet02() {
         val quotationEntityList: MutableList<QuotationEntity> = ArrayList()
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d2", "a2", "q1"
-                ))
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d3", "a2", "q3"
-                ))
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d4", "a4", "q1"
-                ))
+        quotationEntityList.add(QuotationEntity("d2", "a2", "q1"))
+        quotationEntityList.add(QuotationEntity("d3", "a2", "q3"))
+        quotationEntityList.add(QuotationEntity("d4", "a4", "q1"))
         databaseRepositoryDouble.insertQuotations(quotationEntityList)
     }
 
     fun insertTestDataSet03() {
         val quotationEntityList: MutableList<QuotationEntity> = ArrayList()
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d5", "a5", "q1"
-                ))
-        quotationEntityList.add(
-                QuotationEntity(
-                        "d6", "a2", "q6"
-                ))
+        quotationEntityList.add(QuotationEntity("d5", "a5", "q1"))
+        quotationEntityList.add(QuotationEntity("d6", "a2", "q6"))
         databaseRepositoryDouble.insertQuotations(quotationEntityList)
     }
 
