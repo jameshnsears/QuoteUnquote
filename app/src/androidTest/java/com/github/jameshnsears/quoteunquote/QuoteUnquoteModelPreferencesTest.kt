337afd32baa2d06bfff0dcd5f7168e3c3075545e
James Sears <james.hn.sears@gmail.com>
2021-02-14 14:26:37 +0200

    fix test

--- app/src/androidTest/java/com/github/jameshnsears/quoteunquote/QuoteUnquoteModelPreferencesTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/quoteunquote/QuoteUnquoteModelPreferencesTest.kt
@@ -9,6 +9,6 @@ class QuoteUnquoteModelPreferencesTest : QuoteUnquoteModelUtility() {
         insertTestDataSet01()
 
         assertEquals("", quoteUnquoteModelDouble.preferencesAuthorSearch(1))
-        assertEquals("", quoteUnquoteModelDouble.preferencesTextSearch(1))
+        assertEquals("q1", quoteUnquoteModelDouble.preferencesTextSearch(1))
     }
 }
