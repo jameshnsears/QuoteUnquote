d24e2d6556e086007dc56aa6ee93039d4f66bfd2
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-18 15:15:19 +0200

    verbose logging

--- bin/coverage-codecov.sh
+++ bin/coverage-codecov.sh
@@ -1,10 +1,10 @@
 # publish coverage to covecov.io - requires device to be connected, but much faster & more reliable than external CI
 
+# NOTE: branch needs to have been pushed to GitHub
+
 export GIT_HEAD_HASH=`git rev-parse HEAD`
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 export CODECOV_UPLOADER_NAME=`hostname`
-
-export GITHUB_ACTIONS="codecov"
 
 ./gradlew clean :app:testGoogleplayDebugCoverage --stacktrace
 bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
