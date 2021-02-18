81840f8205180c4a5cef9e1fac891d493dc18858
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-18 15:13:15 +0200

    verbose logging

--- bin/coverage-codecov.sh
+++ bin/coverage-codecov.sh
@@ -4,11 +4,13 @@ export GIT_HEAD_HASH=`git rev-parse HEAD`
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 export CODECOV_UPLOADER_NAME=`hostname`
 
+export GITHUB_ACTIONS="codecov"
+
 ./gradlew clean :app:testGoogleplayDebugCoverage --stacktrace
-bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
+bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
 
 ./gradlew clean :app:testManifestTestDebugCoverage --stacktrace
-bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/ManifestTestDebug.xml -F app.manifestTest
+bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/ManifestTestDebug.xml -F app.manifestTest
 
 ./gradlew clean :app:testFdroidDebugCoverage --stacktrace
-bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/FdroidDebug.xml -F app.froid
+bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/FdroidDebug.xml -F app.froid
