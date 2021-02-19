05e2a34900b3ef76044a7106476b889c03576c8c
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-19 16:21:13 +0200

    rm manifestTest out of visibility as no way for gradle to get to it!

--- bin/coverage-codecov.sh
+++ bin/coverage-codecov.sh
@@ -9,8 +9,5 @@ export CODECOV_UPLOADER_NAME=`hostname`
 ./gradlew clean :app:testGoogleplayDebugCoverageCombined --stacktrace
 bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
 
-./gradlew clean :app:testManifestTestDebugCoverage --stacktrace
-bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/ManifestTestDebug.xml -F app.manifestTest
-
 ./gradlew clean :app:testFdroidDebugCoverageCombined --stacktrace
 bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/FdroidDebug.xml -F app.froid
