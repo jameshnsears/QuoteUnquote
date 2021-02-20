f5063c2273bcb46a564921d2ea69c4da43064435
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-20 14:59:48 +0200

    wip

--- bin/coverage-codecov.sh
+++ bin/coverage-codecov.sh
@@ -1,13 +1,18 @@
 # publish coverage to covecov.io - requires device to be connected, but much faster & more reliable than external CI
-
-# NOTE: branch needs to have been pushed to GitHub for codecov.io to show src
+#
+# NOTE:
+#   - branch needs to have been pushed to GitHub for codecov.io to show src
+#   - run from project root folder
 
 export GIT_HEAD_HASH=`git rev-parse HEAD`
 export GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 export CODECOV_UPLOADER_NAME=`hostname`
 
-./gradlew clean :app:testGoogleplayDebugCoverageCombined --stacktrace
-bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
-
-./gradlew clean :app:testFdroidDebugCoverageCombined --stacktrace
-bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/FdroidDebug.xml -F app.froid
+#./gradlew clean :app:testManifestTestDebugCoverage --stacktrace
+#bash <(curl https://codecov.io/bash) -v -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/ManifestTestDebug.xml -F app.manifestTest
+#
+#./gradlew clean :app:testGoogleplayDebugCoverageCombined --stacktrace
+#bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/GoogleplayDebug.xml -F app.gooleplay
+#
+#./gradlew clean :app:testFdroidDebugCoverageCombined --stacktrace
+#bash <(curl https://codecov.io/bash) -t ${CODECOVIO_TOKEN} -C ${GIT_HEAD_HASH} -B ${GIT_BRANCH} -n ${CODECOV_UPLOADER_NAME} -f app/build/reports/FdroidDebug.xml -F app.froid
