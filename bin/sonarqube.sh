623d4ac5e451c4ef33f5311f34bfe3d9f03e4003
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-24 17:52:25 +0200

    local sonarque

---
+++ bin/sonarqube.sh
@@ -0,0 +1,17 @@
+# https://docs.sonarqube.org/latest/setup/get-started-2-minutes/
+
+# docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
+#
+# http://localhost:9000
+# admin/admin
+
+# docker network inspect bridge | grep IPv4Address
+
+./gradlew clean assembleDebug
+
+docker run \
+--rm \
+-e SONAR_HOST_URL="http://172.17.0.2:9000" \
+-e SONAR_LOGIN=d4e1a021d8aa07c7b47bca0f699314d4915bd790 \
+-v "$HOME/GIT_REPOS/QuoteUnquote:/usr/src" \
+sonarsource/sonar-scanner-cli
