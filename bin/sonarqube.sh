c7eba400dcad7976c5ab34f0459417b2db862785
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-24 18:24:07 +0200

    shellcheck fixes

--- bin/sonarqube.sh
+++ bin/sonarqube.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # https://docs.sonarqube.org/latest/setup/get-started-2-minutes/
 
 # docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAP_CHECKS_DISABLE=true -p 9000:9000 sonarqube:latest
