6063df9be883f5d7f0a7080fd0957f2ad3bcedf6
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-26 19:12:41 +0200

    ktlintFormat

--- app/src/androidTest/java/com/github/jameshnsears/quoteunquote/WidgetToolbarFavouriteTest.kt
+++ app/src/androidTest/java/com/github/jameshnsears/quoteunquote/WidgetToolbarFavouriteTest.kt
@@ -20,7 +20,7 @@ class WidgetToolbarFavouriteTest : QuoteUnquoteModelUtility() {
         assertTrue(quoteUnquoteModelDouble.countFavourites() == 1)
 
         quoteUnquoteModelDouble.toggleFavourite(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, DatabaseRepository.DEFAULT_QUOTATION_DIGEST
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, DatabaseRepository.DEFAULT_QUOTATION_DIGEST
         )
 
         assertTrue(quoteUnquoteModelDouble.countFavourites() == 0)
@@ -36,33 +36,33 @@ class WidgetToolbarFavouriteTest : QuoteUnquoteModelUtility() {
 
         // press 'favourite'
         quoteUnquoteModelDouble.setNext(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
         )
         var digest = quoteUnquoteModelDouble.getNext(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL
         )?.digest
         if (digest != null) {
             quoteUnquoteModelDouble.toggleFavourite(
-                    WidgetIdHelper.INSTANCE_01_WIDGET_ID, digest
+                WidgetIdHelper.INSTANCE_01_WIDGET_ID, digest
             )
             expectedDigestsList.add(digest)
         }
 
         // press 'next'
         quoteUnquoteModelDouble.setNext(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
         )
 
         // press 'favourite'
         quoteUnquoteModelDouble.setNext(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL, false
         )
         digest = quoteUnquoteModelDouble.getNext(
-                WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL
+            WidgetIdHelper.INSTANCE_01_WIDGET_ID, ContentSelection.ALL
         )?.digest
         if (digest != null) {
             quoteUnquoteModelDouble.toggleFavourite(
-                    WidgetIdHelper.INSTANCE_01_WIDGET_ID, digest
+                WidgetIdHelper.INSTANCE_01_WIDGET_ID, digest
             )
             expectedDigestsList.add(digest)
         }
