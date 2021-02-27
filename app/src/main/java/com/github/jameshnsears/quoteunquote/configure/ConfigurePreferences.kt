6dcf4429484531844e0b55386b52437231cdcf78
jsears <55b3c47e60d6c695a5afe356092047f2c94668c8>
2021-02-27 10:39:51 +0200

    ktlint fix

--- app/src/main/java/com/github/jameshnsears/quoteunquote/configure/ConfigurePreferences.kt
+++ app/src/main/java/com/github/jameshnsears/quoteunquote/configure/ConfigurePreferences.kt
@@ -3,14 +3,16 @@ package com.github.jameshnsears.quoteunquote.configure
 import android.content.Context
 import com.github.jameshnsears.quoteunquote.utils.preference.PreferencesFacade
 
-class ConfigurePreferences(widgetId: Int, applicationContext: Context): PreferencesFacade(widgetId, applicationContext) {
-    private val CONFIGURE_SCROLL_Y = "CONFIGURE_SCROLL_Y"
+class ConfigurePreferences(widgetId: Int, applicationContext: Context) :
+    PreferencesFacade(widgetId, applicationContext) {
+
+    private val configureScrollY = "CONFIGURE_SCROLLY"
 
     fun getScrollY(): Int? {
-        return preferenceHelper?.getPreferenceInt(getPreferenceKey(CONFIGURE_SCROLL_Y))
+        return preferenceHelper?.getPreferenceInt(getPreferenceKey(configureScrollY))
     }
 
     fun setScrollY(value: Int) {
-        preferenceHelper?.setPreference(getPreferenceKey(CONFIGURE_SCROLL_Y), value)
+        preferenceHelper?.setPreference(getPreferenceKey(configureScrollY), value)
     }
 }
